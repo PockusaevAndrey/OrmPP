f9ec0bae10547605230025e5489f8f475a850ec8
eldar <anderweys999@gmail.com>
2022-05-23 18:49:32 +0300

    Add Exception: InvalidColumn.hpp, InvalidQuery.hpp.

---
+++ include/LINQ/exception/InvalidColumn.hpp
@@ -0,0 +1,13 @@
+//
+// Created by eldar on 23.05.22.
+//
+
+#ifndef LINQ_INVALIDCOLUMN_HPP
+#define LINQ_INVALIDCOLUMN_HPP
+
+#include "iostream"
+
+class invalid_column : public std::exception {};
+
+#include "iostream"
+#endif //LINQ_INVALIDCOLUMN_HPP
---
+++ include/LINQ/exception/InvalidQuery.hpp
@@ -0,0 +1,13 @@
+//
+// Created by eldar on 23.05.22.
+//
+
+#ifndef LINQ_INVALIDQUERY_HPP
+#define LINQ_INVALIDQUERY_HPP
+
+#include "iostream"
+
+class InvalidQuery : public std::exception {};
+
+
+#endif //LINQ_INVALIDQUERY_HPP
